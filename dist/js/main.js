ab435405e57a8c7c7547e8b0e5f946c7aa41efef
RainJoy <1490079545@qq.com>
2017-06-13 19:22:54 +0800

    调整

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,7 +20,7 @@ if (tab) {
     if (tabCon.length) {
         tab.addEventListener('click', function(e) {
             var value = e.target.getAttribute('value')
-            Array.prototype.slice.call(tabCon).forEach((v, i) => {
+            Array.prototype.slice.call(tabCon).forEach(function(v, i) {
                 if (Number(value) === i) {
                     tab.querySelectorAll('i')[i].setAttribute('class', 'on')
                     v.style.display = 'block'
@@ -40,7 +40,7 @@ if (button) {
     if (buttonCon.length) {
         button.addEventListener('click', function(e) {
             var value = e.target.getAttribute('value')
-            Array.prototype.slice.call(buttonCon).forEach((v, i) => {
+            Array.prototype.slice.call(buttonCon).forEach(function(v, i) {
                 if (Number(value) === i) {
                     v.style.display = 'block'
                 } else {
@@ -51,7 +51,7 @@ if (button) {
         document.body.addEventListener('click', function(e) {
             var _class = e.target.getAttribute('class')
             if (_class !== 'links-item-button') {
-                Array.prototype.slice.call(buttonCon).forEach((v) => {
+                Array.prototype.slice.call(buttonCon).forEach(function(v) {
                     v.style.display = 'none'
                 })
             }
@@ -64,7 +64,7 @@ var time = document.querySelectorAll('.science-getTime .Article_PublishDate')
 if (time.length) {
     var txt = document.querySelectorAll('.science .div_piccontenttitle_li .c_txt')
     if (txt.length) {
-        Array.prototype.slice.call(time).forEach((v, i) => {
+        Array.prototype.slice.call(time).forEach(function(v, i) {
             var el = document.createElement('div')
             el.setAttribute('class', 'publish-time')
             el.innerHTML = v.innerHTML
@@ -87,7 +87,7 @@ if (search && form) {
 // location icon
 var icon = document.querySelectorAll('.listcolumnn .right .possplit')
 if (icon.length) {
-    Array.prototype.slice.call(icon).forEach((v) => {
+    Array.prototype.slice.call(icon).forEach(function(v) {
         v.innerHTML = '>'
     })
 }
@@ -138,3 +138,52 @@ if (favorite) {
         addFavorite()
     })
 }
+
+// link show
+var oldLink = document.querySelector('.header .tool .show-link')
+if (oldLink) {
+    var oldLinkMenu = document.querySelector('.header .tool .show-link .show-link-menu')
+    oldLink.addEventListener('click', function(e) {
+        oldLinkMenu.style.display = 'block'
+    })
+    document.body.addEventListener('click', function(e) {
+        var _value = e.target.getAttribute('value')
+        if (_value !== 'show-link') {
+            oldLinkMenu.style.display = 'none'
+        }
+    })
+}
+
+setTimeout(function linkAndChange() {
+    // pic news to link
+    var picNews = document.querySelectorAll('.module1-content .pic .picbase1-slidenav img')
+    var picLink = document.querySelectorAll('.module1-content .getPicLink a')
+    var picSlidenav = document.querySelectorAll('.module1-content .pic .picbase1-slidenav li')
+    function getPicLink(i) {
+        var v = picNews[i]
+        var html = '<a href="'+ picLink[i].href +'"><img style="width: 550px;height: 430px;" src="' + v.src.replace('_s.', '.') + '" alt="' + v.alt + '"><div class="picbase1-content"><h2>' + v.alt + '</h2></div></a>'
+        document.querySelector('.module1-content .pic .picbase1').innerHTML = html
+    }
+    var picNewIndex = 0
+    var len = picNews.length
+    if (len && picLink.length) {
+        getPicLink(picNewIndex)
+        setInterval(function() {
+            if (picNewIndex === (len - 1)) {
+                picNewIndex = 0
+            } else {
+                picNewIndex ++
+            }
+            getPicLink(picNewIndex)
+            if (picSlidenav.length) {
+                Array.prototype.slice.call(picSlidenav).forEach(function(v, i) {
+                    if (picNewIndex === i) {
+                        v.setAttribute('class', 'active')
+                    } else {
+                        v.setAttribute('class', '')
+                    }
+                })
+            }
+        }, 3000)
+    }
+}, 1000)
